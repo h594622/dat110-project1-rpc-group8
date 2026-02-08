478a0e4c2f28b89b08f2569f996c7eba6c960d84
Trude Svardal <trude_sva@hotmail.com>
2026-02-08 10:46:37 +0100

    Endring av navn på branch

--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -7,24 +7,31 @@ import no.hvl.dat110.TODO;
 public class MessageUtils {
 
 	public static final int SEGMENTSIZE = 128;
-
 	public static int MESSAGINGPORT = 8080;
 	public static String MESSAGINGHOST = "localhost";
 
 	public static byte[] encapsulate(Message message) {
 		
 		byte[] segment = null;
-		byte[] data;
+		byte[] data = new byte[0];
 		
 		// TODO - START
 		
 		// encapulate/encode the payload data of the message and form a segment
 		// according to the segment format for the messaging layer
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
+
+		data = message.getData();
+
+		segment = new byte[SEGMENTSIZE];
+		segment[0] = (byte) data.length;
+
+		System.arraycopy(data, 0, segment, 1, data.length);
+
+/*		if (true)
+			throw new UnsupportedOperationException(TODO.method()); */
+
 		// TODO - END
+
 		return segment;
 		
 	}
@@ -35,9 +42,17 @@ public class MessageUtils {
 		
 		// TODO - START
 		// decapsulate segment and put received payload data into a message
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
+
+		int length = segment[0] & 0xFF;
+
+		byte[] data = new byte[length];
+
+		System.arraycopy(segment, 1, data, 0, length);
+
+		message = new Message(data);
+
+/*		if (true)
+			throw new UnsupportedOperationException(TODO.method()); */
 		
 		// TODO - END
 		
